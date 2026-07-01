005adeb77412b5ef5e5684302615aa2c5c539b80
Muskan Singh <420royalrajput50806@gmail.com>
2026-07-01 23:00:11 +0530

    Fix model filename casing

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,9 +2,7 @@
 import { asynchandler } from "../utils/asynchandler.js";
 import { apires } from "../utils/apires.js";
 import { apierror } from "../utils/apierror.js";
-
-// correct
-import User from '../models/user.model.js'
+import User from '../models/User.model.js'
 const generateAccessTokenAndRefreshToken = async(userId) => {
     try {
         const user = await User.findById(userId);
